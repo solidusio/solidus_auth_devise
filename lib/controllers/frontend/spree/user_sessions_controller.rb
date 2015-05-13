d6d2728dc09cbead9c28b596d51e3dbcbdc166dd
Chris Todorov <chris@freerunningtechnologies.com>
2015-05-12 20:03:15 -0700

    Refactor UserSessionsController and specs

--- lib/controllers/frontend/spree/user_sessions_controller.rb
+++ lib/controllers/frontend/spree/user_sessions_controller.rb
@@ -18,36 +18,44 @@ class Spree::UserSessionsController < Devise::SessionsController
 
     if spree_user_signed_in?
       respond_to do |format|
-        format.html {
+        format.html do
           flash[:success] = Spree.t(:logged_in_succesfully)
           redirect_back_or_default(after_sign_in_path_for(spree_current_user))
-        }
-        format.js {
-          render :json => {:user => spree_current_user,
-                           :ship_address => spree_current_user.ship_address,
-                           :bill_address => spree_current_user.bill_address}.to_json
-        }
+        end
+        format.js { render success_json }
       end
     else
       respond_to do |format|
-        format.html {
+        format.html do
           flash.now[:error] = t('devise.failure.invalid')
           render :new
-        }
-        format.js {
-          render :json => { error: t('devise.failure.invalid') }, status: :unprocessable_entity
-        }
+        end
+        format.js do
+          render json: { error: t('devise.failure.invalid') },
+            status: :unprocessable_entity
+        end
       end
     end
   end
 
   private
-    def accurate_title
-      Spree.t(:login)
-    end
 
-    def redirect_back_or_default(default)
-      redirect_to(session["spree_user_return_to"] || default)
-      session["spree_user_return_to"] = nil
-    end
+  def accurate_title
+    Spree.t(:login)
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session["spree_user_return_to"] || default)
+    session["spree_user_return_to"] = nil
+  end
+
+  def success_json
+    {
+      json: {
+        user: spree_current_user,
+        ship_address: spree_current_user.ship_address,
+        bill_address: spree_current_user.bill_address
+      }.to_json
+    }
+  end
 end
