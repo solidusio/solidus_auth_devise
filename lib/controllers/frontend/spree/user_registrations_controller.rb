030d7762f875135b245666b8d7fefa9fd54584c6
Chris Todorov <chris@freerunningtechnologies.com>
2015-05-25 15:21:38 -0700

    Remove empty overrides in registrations controller

--- lib/controllers/frontend/spree/user_registrations_controller.rb
+++ lib/controllers/frontend/spree/user_registrations_controller.rb
@@ -21,7 +21,6 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
     @user = resource
   end
 
-  # POST /resource/sign_up
   def create
     @user = build_resource(spree_user_params)
     if resource.save
@@ -34,30 +33,6 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
       clean_up_passwords(resource)
       render :new
     end
-  end
-
-  # GET /resource/edit
-  def edit
-    super
-  end
-
-  # PUT /resource
-  def update
-    super
-  end
-
-  # DELETE /resource
-  def destroy
-    super
-  end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  def cancel
-    super
   end
 
   protected
