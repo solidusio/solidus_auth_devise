953d1ddffec1e7b7d3290e10c8abbf9f02c98a2c
Richard Wilson <richard@freerunningtech.com>
2015-07-13 12:02:24 -0700

    Explicitly include UserMethods on Spree::User
    UserMethods is automatically included on Spree.user_class as a
    to_prepare on Spree::Core::Engine within solidus. However, because
    certain specs will manually reload the User model, this include and any
    other preloaded information for the User is lost.

--- spec/support/confirm_helpers.rb
+++ spec/support/confirm_helpers.rb
@@ -1,5 +1,7 @@
 module ConfirmHelpers
   def set_confirmable_option(value)
+    # FIXME: This is terrible terribleness and causes anything loaded
+    # on the user at initialization to be lost.
     Spree::Auth::Config[:confirmable] = value
     Spree.send(:remove_const, 'User')
     load File.expand_path("../../../app/models/spree/user.rb", __FILE__)
