93e830a4b6391c9cba88fd8b4ab4678decd705ef
Brian Buchalter <bal711@gmail.com>
2016-04-23 16:07:14 -0600

    Make route syntax consistent and modern
    This brings the route definition syntax up to modern standards by using
    the `to` option.

--- config/routes.rb
+++ config/routes.rb
@@ -16,20 +16,20 @@ Spree::Core::Engine.add_routes do
     resources :users, :only => [:edit, :update]
 
     devise_scope :spree_user do
-      get '/login' => 'user_sessions#new', :as => :login
-      post '/login' => 'user_sessions#create', :as => :create_new_session
-      get '/logout' => 'user_sessions#destroy', :as => :logout
-      get '/signup' => 'user_registrations#new', :as => :signup
-      post '/signup' => 'user_registrations#create', :as => :registration
-      get '/password/recover' => 'user_passwords#new', :as => :recover_password
-      post '/password/recover' => 'user_passwords#create', :as => :reset_password
-      get '/password/change' => 'user_passwords#edit', :as => :edit_password
-      put '/password/change' => 'user_passwords#update', :as => :update_password
-      get '/confirm' => 'user_confirmations#show', :as => :confirmation if Spree::Auth::Config[:confirmable]
+      get '/login', :to => 'user_sessions#new', :as => :login
+      post '/login', :to => 'user_sessions#create', :as => :create_new_session
+      get '/logout', :to => 'user_sessions#destroy', :as => :logout
+      get '/signup', :to => 'user_registrations#new', :as => :signup
+      post '/signup', :to => 'user_registrations#create', :as => :registration
+      get '/password/recover', :to => 'user_passwords#new', :as => :recover_password
+      post '/password/recover', :to => 'user_passwords#create', :as => :reset_password
+      get '/password/change', :to => 'user_passwords#edit', :as => :edit_password
+      put '/password/change', :to => 'user_passwords#update', :as => :update_password
+      get '/confirm', :to => 'user_confirmations#show', :as => :confirmation if Spree::Auth::Config[:confirmable]
     end
 
-    get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
-    put '/checkout/registration' => 'checkout#update_registration', :as => :update_checkout_registration
+    get '/checkout/registration', :to => 'checkout#registration', :as => :checkout_registration
+    put '/checkout/registration', :to => 'checkout#update_registration', :as => :update_checkout_registration
 
     resource :account, :controller => 'users'
   end
@@ -49,9 +49,9 @@ Spree::Core::Engine.add_routes do
 
       devise_scope :spree_user do
         get '/authorization_failure', :to => 'user_sessions#authorization_failure', :as => :unauthorized
-        get '/login' => 'user_sessions#new', :as => :login
-        post '/login' => 'user_sessions#create', :as => :create_new_session
-        get '/logout' => 'user_sessions#destroy', :as => :logout
+        get '/login', :to => 'user_sessions#new', :as => :login
+        post '/login', :to => 'user_sessions#create', :as => :create_new_session
+        get '/logout', :to => 'user_sessions#destroy', :as => :logout
       end
     end
   end
