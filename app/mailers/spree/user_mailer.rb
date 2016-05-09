a53b0a251f53fe2a8752b8fb0a7c529f968794d0
Andrew Stewart <andrew@stwrt.ca>
2016-05-09 13:56:30 -0400

    Update hash syntax for mailer and configuration
    This commits converts the Spree::UserMailer mailer class to the new Ruby
    1.9+ hash syntax, along with the Spree::AuthConfiguration class.
    
    This is as part of a larger set of commits to update the code style for
    auth_devise to reflect current standards.

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -1,15 +1,15 @@
 module Spree
   class UserMailer < BaseMailer
     def reset_password_instructions(user, token, *args)
-      @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Spree::Store.current.url)
+      @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
       @store = Spree::Store.current
-      mail to: user.email, from: from_address(@store), subject: "#{@store.name} #{I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])}"
+      mail to: user.email, from: from_address(@store), subject: "#{@store.name} #{I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions])}"
     end
 
     def confirmation_instructions(user, token, opts={})
-      @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Spree::Store.current.url)
+      @confirmation_url = spree.spree_user_confirmation_url(confirmation_token: token, host: Spree::Store.current.url)
       @store = Spree::Store.current
-      mail to: user.email, from: from_address(@store), subject: "#{@store.name} #{I18n.t(:subject, :scope => [:devise, :mailer, :confirmation_instructions])}"
+      mail to: user.email, from: from_address(@store), subject: "#{@store.name} #{I18n.t(:subject, scope: [:devise, :mailer, :confirmation_instructions])}"
     end
   end
 end
