fc750e048d7a81d0009ddf351d08c897d832bdf5
Andrew Stewart <andrew@stwrt.ca>
2017-05-22 13:47:26 +0100

    Only apply frontend override if frontend present
    Closes #85.
    
    This commit updates the Deface override providing the shared login bar
    for solidus_frontend to only do so if solidus_frontend is present. This
    prevents compilation errors from Deface in stores that do not include
    solidus_frontend.

--- app/overrides/auth_shared_login_bar.rb
+++ app/overrides/auth_shared_login_bar.rb
@@ -1,8 +1,10 @@
-Deface::Override.new(
-  virtual_path: "spree/shared/_nav_bar",
-  name: "auth_shared_login_bar",
-  insert_before: "li#search-bar",
-  partial: "spree/shared/login_bar",
-  disabled: false,
-  original: 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac'
-)
+if Spree::Auth::Engine.frontend_available?
+  Deface::Override.new(
+    virtual_path: "spree/shared/_nav_bar",
+    name: "auth_shared_login_bar",
+    insert_before: "li#search-bar",
+    partial: "spree/shared/login_bar",
+    disabled: false,
+    original: 'eb3fa668cd98b6a1c75c36420ef1b238a1fc55ac'
+  )
+end
