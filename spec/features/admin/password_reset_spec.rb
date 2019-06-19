f477dcd54fa4278b18cd1dd99ccec41b1801d191
jacobherrington <jacobherringtondeveloper@gmail.com>
2019-06-18 19:58:14 -0500

    Remove flash errors from admin password recovery
    It is possible for an attacker to deduce admin account email addresses
    by injecting email addresses into the password recovery view until they
    receive a success message.
    
    The solution in this commit is to remove those flash error messages from
    that view entirely. Instead we will flash a generic success message when
    the form submits.
    
    This functionality will still exist for non-admin users who may be less
    technically savvy and may need help remembering the email address they
    used to sign up.

--- spec/features/admin/password_reset_spec.rb
+++ spec/features/admin/password_reset_spec.rb
@@ -7,19 +7,31 @@ RSpec.feature 'Admin - Reset Password', type: :feature do
     ActionMailer::Base.default_url_options[:host] = 'http://example.com'
   end
 
-  scenario 'allows a user to supply an email for the password reset' do
-    create(:user, email: 'foobar@example.com', password: 'secret', password_confirmation: 'secret')
-    visit spree.admin_login_path
-    click_link 'Forgot Password?'
-    fill_in 'Email', with: 'foobar@example.com'
-    click_button 'Reset my password'
-    expect(page).to have_text 'You will receive an email with instructions'
+  context 'when an account with this email address exists' do
+    let!(:user) { create(:user, email: 'foobar@example.com', password: 'secret', password_confirmation: 'secret') }
+
+    scenario 'allows a user to supply an email for the password reset' do
+      visit spree.admin_login_path
+      click_link 'Forgot Password?'
+      fill_in_email
+      click_button 'Reset my password'
+      expect(page).to have_text 'you will receive an email with instructions'
+    end
   end
 
-  scenario 'shows errors if no email is supplied' do
+  # Revealing that an admin email address is not found allows an attacker to
+  # find admin account email addresses by trying email addresses until this
+  # error is not shown.
+  scenario 'does not reveal email addresses if they are not found' do
     visit spree.admin_login_path
     click_link 'Forgot Password?'
+    fill_in_email
     click_button 'Reset my password'
-    expect(page).to have_text "Email can't be blank"
+    expect(page).to_not have_text "Email not found"
+    expect(page).to have_text 'you will receive an email with instructions'
+  end
+
+  def fill_in_email
+    fill_in 'Email', with: 'foobar@example.com'
   end
 end
