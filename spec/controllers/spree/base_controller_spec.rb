ff57ff526adffdf5840e83f254caadbd27229c15
Alberto Vena <kennyadsl@gmail.com>
2020-05-20 17:03:07 +0200

    Try to redirect back on unauthorized accesses
    Only when the `redirect_back_on_unauthorized` preference
    exists and is set to true.
    
    This preference has been introduced in core with solidusio/solidus#3118
    and we can rely on that preference to drive the behavior change here
    as well.
    
    The extra
    
    	if Spree::Config.respond_to?(:redirect_back_on_unauthorized)
    
    check might seem useless but it's needed to avoid printing this
    deprecation warning on Solidus versions that still do not have
    the preference.
    
    If the Solidus verion used does not have the preference yet, the old
    behavior will be preserved.

--- spec/controllers/spree/base_controller_spec.rb
+++ spec/controllers/spree/base_controller_spec.rb
@@ -8,19 +8,45 @@ RSpec.describe Spree::BaseController, type: :controller do
       def index; authorize!(:read, :something); end
     end
 
+    before do
+      stub_spree_preferences(Spree::Config, redirect_back_on_unauthorized: true)
+    end
+
     context "when user is logged in" do
       before { sign_in(create(:user)) }
 
-      it "redirects to unauthorized path" do
-        get :index
-        expect(response).to redirect_to(spree.unauthorized_path)
+      context "when http_referrer is not present" do
+        it "redirects to unauthorized path" do
+          get :index
+          expect(response).to redirect_to(spree.unauthorized_path)
+        end
+      end
+
+      context "when http_referrer is present" do
+        before { request.env['HTTP_REFERER'] = '/redirect' }
+
+        it "redirects back" do
+          get :index
+          expect(response).to redirect_to('/redirect')
+        end
       end
     end
 
     context "when user is not logged in" do
-      it "redirects to login path" do
-        get :index
-        expect(response).to redirect_to(spree.login_path)
+      context "when http_referrer is not present" do
+        it "redirects to login path" do
+          get :index
+          expect(response).to redirect_to(spree.login_path)
+        end
+      end
+
+      context "when http_referrer is present" do
+        before { request.env['HTTP_REFERER'] = '/redirect' }
+
+        it "redirects back" do
+          get :index
+          expect(response).to redirect_to('/redirect')
+        end
       end
     end
   end
