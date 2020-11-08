481262c0485a4f34cdca5f385fc1dd0e1f7b5a15
brchristian <brchristian@gmail.com>
2020-11-07 21:57:05 -0800

    Update migrations to support Rails 5.1

--- db/migrate/20200417153503_add_unconfirmed_email_to_spree_users.rb
+++ db/migrate/20200417153503_add_unconfirmed_email_to_spree_users.rb
@@ -1,4 +1,4 @@
-class AddUnconfirmedEmailToSpreeUsers < ActiveRecord::Migration[5.2]
+class AddUnconfirmedEmailToSpreeUsers < SolidusSupport::Migration[5.1]
   def change
     unless column_exists?(:spree_users, :unconfirmed_email)
       add_column :spree_users, :unconfirmed_email, :string
