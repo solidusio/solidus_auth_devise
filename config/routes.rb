41dd99adf71e0a2bd42c77b5cba0b5797242d634
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2022-09-22 13:51:20 -0600

    Explicitly declare devise generated routes
    We have some duplication affecting all stores using this extension
    and this is the first step toward removing it.
    
    E.g. the sign_in path was available at both /login and /spree_user/sign_in
    as `login_path` or `new_spree_user_session_path`.
    
    Co-Authored-By: Elia Schito <elia@schito.me>

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,30 @@ Spree::Core::Engine.routes.draw do
         passwords: 'spree/user_passwords',
         confirmations: 'spree/user_confirmations'
       },
-      skip: [:unlocks, :omniauth_callbacks],
-      path_names: { sign_out: 'logout' },
-      path_prefix: :user,
-      router_name: :spree
+      skip: :all,
     })
 
     resources :users, only: [:edit, :update]
 
     devise_scope :spree_user do
+      # Legacy devise generated routes
+      get 'user/spree_user/sign_in', to: 'user_sessions#new', as: :new_spree_user_session
+      post 'user/spree_user/sign_in', to: 'user_sessions#create', as: :spree_user_session
+      match 'user/spree_user/logout', to: 'user_sessions#destroy', as: :destroy_spree_user_session, via: Devise.sign_out_via
+      get 'user/spree_user/password/new', to: 'user_passwords#new', as: :new_spree_user_password
+      get 'user/spree_user/password/edit', to: 'user_passwords#edit', as: :edit_spree_user_password
+      patch 'user/spree_user/password', to: 'user_passwords#update', as: :spree_user_password
+      put 'user/spree_user/password', to: 'user_passwords#update'
+      post 'user/spree_user/password', to: 'user_passwords#create'
+      get 'user/spree_user/cancel', to: 'user_registrations#cancel', as: :cancel_spree_user_registration
+      get 'user/spree_user/sign_up', to: 'user_registrations#new', as: :new_spree_user_registration
+      get 'user/spree_user/edit', to: 'user_registrations#edit', as: :edit_spree_user_registration
+      patch 'user/spree_user', to: 'user_registrations#update', as: :spree_user_registration
+      put 'user/spree_user', to: 'user_registrations#update', as: nil
+      delete 'user/spree_user', to: 'user_registrations#destroy', as: nil
+      post 'user/spree_user', to: 'user_registrations#create', as: nil
+
+      # Custom devise routes
       get '/login', to: 'user_sessions#new', as: :login
       post '/login', to: 'user_sessions#create', as: :create_new_session
       match '/logout', to: 'user_sessions#destroy', as: :logout, via: Devise.sign_out_via
