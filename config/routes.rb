bd2e85433ca8e8f4f59ae52a108179d42a38d3a7
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2022-09-29 11:19:22 -0600

    Only use custom routes
    Using both devise generated routes and custom ones is confusing.
    
    As an example: when failing authentication, the devise route would
    send the user to "new_spree_user_session_path", however we want our
    users to be redirected to "/login." This commit deprecates the route
    and sends users to "/login."

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Spree::Core::Engine.routes.draw do
         passwords: 'spree/user_passwords',
         confirmations: 'spree/user_confirmations'
       },
-      skip: [:unlocks, :omniauth_callbacks],
+      skip: :all,
       path_names: { sign_out: 'logout' },
       path_prefix: :user,
       router_name: :spree
@@ -19,16 +19,44 @@ Spree::Core::Engine.routes.draw do
     resources :users, only: [:edit, :update]
 
     devise_scope :spree_user do
-      get '/login', to: 'user_sessions#new', as: :login
-      post '/login', to: 'user_sessions#create', as: :create_new_session
-      match '/logout', to: 'user_sessions#destroy', as: :logout, via: Devise.sign_out_via
-      get '/signup', to: 'user_registrations#new', as: :signup
-      post '/signup', to: 'user_registrations#create', as: :registration
-      get '/password/recover', to: 'user_passwords#new', as: :recover_password
-      post '/password/recover', to: 'user_passwords#create', as: :reset_password
-      get '/password/change', to: 'user_passwords#edit', as: :edit_password
-      put '/password/change', to: 'user_passwords#update', as: :update_password
-      get '/confirm', to: 'user_confirmations#show', as: :confirmation if Spree::Auth::Config[:confirmable]
+      # Custom devise routes
+      get   '/login', to: 'user_sessions#new', as: :login
+      post  '/login', to: 'user_sessions#create', as: :create_new_session
+      match '/logout', to: 'user_sessions#destroy', via: Devise.sign_out_via, as: :logout
+      get   '/password/recover', to: 'user_passwords#new', as: :recover_password
+      get   '/password/change', to: 'user_passwords#edit', as: :edit_password
+      put   '/password/change', to: 'user_passwords#update', as: :update_password
+      post  '/password/recover', to: 'user_passwords#create', as: :reset_password
+      get   '/signup', to: 'user_registrations#new', as: :signup
+      post  '/signup', to: 'user_registrations#create', as: :registration
+      get   '/confirm', to: 'user_confirmations#show', as: :confirmation if Spree::Auth::Config[:confirmable]
+
+      # Legacy devise generated paths
+      #
+      # These are deprecated but we still want to support the incoming routes, in order to give existing stores an upgrade path.
+      # Will be removed at the next major release of solidus_auth_devise.
+      get    '/user/spree_user/password/edit' => 'user_passwords#edit', as: :deprecated_edit_spree_user_password
+      get    '/password/change'               => 'user_passwords#edit', as: :edit_spree_user_password
+      get    '/user/spree_user/password/new'  => 'user_passwords#new', as: :deprecated_new_spree_user_password
+      get    '/password/recover'              => 'user_passwords#new', as: :new_spree_user_password
+      match  '/user/spree_user/password'      => 'user_passwords#update', via: [:patch, :put], as: :deprecated_spree_user_password
+      put    '/password/change'               => 'user_passwords#update', as: :spree_user_password
+      post   '/user/spree_user/password'      => 'user_passwords#create', as: nil
+
+      get    '/login'                         => 'user_sessions#new', as: :new_spree_user_session
+      get    '/user/spree_user/sign_in'       => 'user_sessions#new', as: :deprecated_new_spree_user_session
+      match  '/user/spree_user/logout'        => 'user_sessions#destroy', via: Devise.sign_out_via, as: :deprecated_destroy_spree_user_session
+      match  '/logout'                        => 'user_sessions#destroy', via: Devise.sign_out_via, as: :destroy_spree_user_session
+      post   '/user/spree_user/sign_in'       => 'user_sessions#create', as: :deprecated_spree_user_session
+      post   '/login'                         => 'user_sessions#create', as: :spree_user_session
+
+      get    '/user/spree_user/sign_up'       => 'user_registrations#new', as: :deprecated_new_spree_user_registration
+      get    '/signup'                        => 'user_registrations#new', as: :new_spree_user_registration
+      post   '/user/spree_user'               => 'user_registrations#create', as: nil
+      get    '/user/spree_user/cancel'        => 'user_registrations#cancel', as: :cancel_spree_user_registration
+      get    '/user/spree_user/edit'          => 'user_registrations#edit', as: :edit_spree_user_registration
+      delete '/user/spree_user'               => 'user_registrations#destroy', as: nil
+      match  '/user/spree_user'               => 'user_registrations#update', as: :spree_user_registration, via: [:patch, :put]
     end
 
     get '/checkout/registration', to: 'checkout#registration', as: :checkout_registration
