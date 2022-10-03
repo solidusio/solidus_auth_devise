c14fc1d852a49ca804334af95d309f28405c8697
Elia Schito <elia@schito.me>
2022-10-03 11:53:55 +0200

    Fix the standard generator alias
    This generator is here for anyone trying to run th standard
    `bin/rails g solidus_auth_devise:install`. Turns out that assigning
    to another constant is not enough and we need to proxy it more
    explicitly.

--- lib/generators/solidus_auth_devise/install/install_generator.rb
+++ lib/generators/solidus_auth_devise/install/install_generator.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative '../../solidus/auth/install/install_generator'
+
 module SolidusAuthDevise
   module Generators
-    InstallGenerator = ::Solidus::Auth::Generators::InstallGenerator
+    class InstallGenerator < Rails::Generators::Base
+      # Copy over any class option from the legacy install generator
+      Solidus::Auth::Generators::InstallGenerator.class_options.each do |name, option|
+        class_options[name] ||= option.dup
+      end
+
+      def forward_to_spree_auth_install
+        generate 'solidus:auth:install', *ARGV
+      end
+    end
   end
 end
