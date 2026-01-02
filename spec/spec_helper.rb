2ec4a45240e9590172a6da5b70a4e3a3509cf01b
Thomas von Deyen <thomas@vondeyen.com>
2026-01-02 23:32:32 +0100

    CI: Run feature tests with Firefox
    Chrome is a buggy piece of...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,12 @@ require dummy_env
 # Requires factories and other useful helpers defined in spree_core.
 require "solidus_dev_support/rspec/feature_helper"
 
+Capybara::Screenshot.register_driver(:selenium_headless) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
+
+Capybara.javascript_driver = :selenium_headless
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir["#{__dir__}/support/**/*.rb"].sort.each { |f| require f }
