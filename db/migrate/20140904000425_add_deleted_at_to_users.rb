cbe75a7aa8fab2e7fbeba55217dca09b7e6b154e
Thomas von Deyen <thomas@vondeyen.com>
2026-01-03 23:47:58 +0100

    Compact migrations
    Some of these migrations are 16 years old.
    It is time to create one migration with all
    changes consolidated.

--- db/migrate/20140904000425_add_deleted_at_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddDeletedAtToUsers < ActiveRecord::Migration[4.2]
-  def change
-    add_column :spree_users, :deleted_at, :datetime
-    add_index :spree_users, :deleted_at
-  end
-end
